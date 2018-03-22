234dc78627cc654fe5011b498eb3d45c30078344
samuel king <samuelr.king314@yahoo.com>
2018-03-21 21:44:11 -0400

    Initial Commit

---
+++ MarsMail/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MarsMail
+//
+//  Created by Sam King on 3/21/18.
+//  Copyright © 2018 Sam King. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
