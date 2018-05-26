31874bbc42c290f7b206bb70634d1be23d390626
Sam King <samking@Sams-MacBook-Air.local>
2018-05-25 19:22:25 -0700

    working on issue 12

--- MarsMail/ViewControllers/EditProfileViewController.swift
+++ MarsMail/ViewControllers/EditProfileViewController.swift
@@ -23,8 +23,27 @@ class EditProfileViewController: UIViewController
     //--- get profile image
     @IBOutlet weak var imgProfPic: UIImageView!
     
-    //--- get text field for name
+    //--- get UITextFields
     @IBOutlet weak var txtName: UITextField!
+    
+    //--- get UILabels
+    @IBOutlet weak var lblTextCount: UITextField!
+    @IBOutlet weak var lblFilesCount: UITextField!
+    @IBOutlet weak var lblVideosCount: UITextField!
+    @IBOutlet weak var lblMarsEmojisCount: UITextField!
+    @IBOutlet weak var lblDBSize: UITextField!
+    @IBOutlet weak var lblPercentLikely: UITextField!
+    
+    //---get test data
+    let imgProfPicPlaceholder = UIImage(named: "marsPlaceholder.jpg")
+    let username = "king_sammmm"
+    let textcount = "20"
+    let filecount = "100"
+    let videocount = "2"
+    let MEcount = "80"
+    let dbSize = "1 / 10^20%"
+    let percentLikely = "60%"
+    
 
     override func viewDidLoad()
     {
@@ -51,6 +70,15 @@ class EditProfileViewController: UIViewController
         //TODO setup DB size info
         
         //TODO setup % likely you are you
+        
+        //for now just use test data
+        imgProfPic.image = imgProfPicPlaceholder
+        txtName.text = username
+        lblTextCount.text = textcount
+        lblFilesCount.text = filecount
+        lblVideosCount.text = videocount
+        lblDBSize.text = dbSize
+        lblPercentLikely.text = percentLikely
     }
     
     //----- UIButton methods
--- MarsMail/ViewControllers/MainViewController.swift
+++ MarsMail/ViewControllers/MainViewController.swift
@@ -14,18 +14,26 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     //--- get tableview
     @IBOutlet weak var tableView: UITableView!
 
-    //--- setup UITableViewCell reuse identifier
+    //--- get UITableViewCell reuse identifier
     let cellReuseIdentifier = "messageCell"
     
-    //--- setup header buttons
+    //--- get header buttons
     @IBOutlet weak var btnEditProfile: UIButton!
     @IBOutlet weak var btnComposeMessage: UIButton!
+    
+    //--- get test data
+    let numOfRows = 3
+    let messageNameList = ["Mars Mission 1 Team", "Mars Mission 1.1", "Craig's Birthday Party"]
+    let messageInfoList = ["hey guys... we're out of water...", "good thing we have water 😎", "did anyone get a cake???"]
+    let messageLastWroteList = ["5m", "2d", "1w"]
+    let messageImageNames = ["marsmission1.jpg","marsmission.jpg","happybday.png"]
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
         tableView.delegate = self
+        tableView.separatorStyle = UITableViewCellSeparatorStyle.none //you can also do .none   
     }
 
     override func didReceiveMemoryWarning()
@@ -38,17 +46,17 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func numberOfSections(in tableView: UITableView) -> Int
     {
-        return 1;
+        return 1
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
     {
-        return 1;
+        return numOfRows //THIS IS FOR TESTING
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat
     {
-        return 80;
+        return 80
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
@@ -58,9 +66,31 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
-        let cell:MessageTableViewCell = self.tableView.dequeueReusableCell( withIdentifier: cellReuseIdentifier ) as! MessageTableViewCell
+        let cell:MessageTableViewCell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! MessageTableViewCell
+        
+        //- setup cell characteristics
+        cell.backgroundColor = UIColor.clear
         
         //TODO Take details from server about message thread and add to cell
+        
+        //for now just using test data, then use test data in data manager then use real server data
+        if (indexPath.row < messageInfoList.count &&
+            indexPath.row < messageNameList.count &&
+            indexPath.row < messageLastWroteList.count &&
+            indexPath.row < messageImageNames.count)
+        {
+            cell.lblThreadName.text = messageNameList[indexPath.row]
+            cell.lblThreadRecentMessage.text = messageInfoList[indexPath.row]
+            cell.lblThreadRecentTime.text = messageLastWroteList[indexPath.row]
+            cell.imageThread.image = UIImage(named: messageImageNames[indexPath.row])
+            
+            //TODO decide whether to put this in async call or nah
+            cell.imageThread.layer.borderWidth = 1.0
+            cell.imageThread.layer.masksToBounds = false
+            cell.imageThread.layer.cornerRadius = cell.imageThread.frame.size.height / 2
+            cell.imageThread.clipsToBounds = true
+        }
+        
         
         return cell
     }
